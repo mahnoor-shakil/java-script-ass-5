b22c0df594dbda8dba573341e91c1f7375bee0ac
mahnoor-shkail <mah.realone@gmail.com>
2024-03-24 01:41:23 +0500

    add one html and one app.js file

---
+++ app.js
@@ -0,0 +1,53 @@
+// string method
+
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your last name:");
+// var fullName = firstName + " " + lastName;
+
+// alert (fullNmae)
+
+// q2
+// var userInput = prompt("enter mobile phone");
+// var inputLength = userInput.length;
+// alert("Length of your favorite mobile phone model: " + inputLength);
+        
+
+// q3
+// var word = "Pakistani";
+//  var index = word.indexOf('n');
+//  if (index !== -1) {
+//  alert("string:"+" "+word + "index of 'n' " + index);
+//  } else {
+//        alert("Letter 'n' not found in the word 'Pakistani'.");
+//      }
+
+// q4
+// var word = "Hello World";
+//  var index = word.indexOf('l');
+//  if (index !== -1) {
+//  alert("index of 'l' " + index);
+//  } else {
+//        alert("Letter 'l' not found in the word 'Hello World'.");
+//      }
+
+// q5
+// var text = "pakistani";
+// console.log(text[3])
+
+// q7
+// var text = "hyderabad";
+// console.log (text.replace('hyder','islam'))
+
+// q8
+// var text = "Ali and Sami are best friends. They play cricket and football together.";
+// console.log (text.replace(/and/g,'&'))
+
+// q9
+            var str = "472";
+            var num = parseInt(str);
+
+            document.write("<b>Original String:</b> " + str + "<br>");
+            document.write("<b>Converted Number:</b> " + num + "<br>");
+            document.write("<b>Type of Original String:</b> " + typeof str + "<br>");
+            document.write("<b>Type of Converted Number:</b> " + typeof num + "<br>");
+        
